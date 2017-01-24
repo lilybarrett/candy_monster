c40bd0a93492a430304090008a69b610afc188eb
lilybarrett <lily@barrett.us>
2017-01-24 10:43:02 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'candies#index'
 
-  resources :candies, only: [:create, :show]
+  resources :candies, only: [:index, :create, :show]
 
   namespace :api do
     namespace :v1 do
