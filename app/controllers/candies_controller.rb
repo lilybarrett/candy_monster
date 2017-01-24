b752fd17ae4c6c4b6bce4ed39bdf57843f3f8a75
lilybarrett <lily@barrett.us>
2017-01-24 10:31:43 -0500

    add form

--- app/controllers/candies_controller.rb
+++ app/controllers/candies_controller.rb
@@ -1,6 +1,18 @@
 class CandiesController < ApplicationController
 
+  def create
+    @candy = Candy.new(candy_params)
+    if @candy.save!
+      flash[:notice] = "Candy successfully added!"
+      redirect_to candy_path(@candy)
+    else
+      flash[:error] = @candy.errors.full_messages.join('. ')
+      render :new
+    end
+  end
+
   def index
+    @candy = Candy.new
     @candies = Candy.all
   end
 
@@ -8,6 +20,9 @@ class CandiesController < ApplicationController
     @candy = Candy.find(params[:id])
   end
 
-  def update
-  end 
+  private
+
+  def candy_params
+    params.require(:candy).permit(:name, :description, :image_url)
+  end
 end
